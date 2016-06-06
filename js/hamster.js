28f8cfdfa51b11452ae424eec61333fea3f0ef06
Alex Swan <BoldBigflank@users.noreply.github.com>
2016-06-05 20:57:39 -0500

    Update hamster.js

--- js/hamster.js
+++ js/hamster.js
@@ -13,11 +13,6 @@
         device.subscribe('distance', function(data){
             $('#distance').html(parseFloat(data.data).toFixed(2));
         });
-        
-        device.subscribe('sound', function(data){
-            $('#sound').html(parseInt(data.data)).toFixed(2));
-        })
-        
     });
 
     var resetData = function(){
